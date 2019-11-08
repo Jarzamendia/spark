e9d219ad8647b96a93b90095b0ef2f94d6015c16
Joao E Arzamendia <jarzamendia@fazenda.ms.gov.br>
2019-11-07 22:42:52 -0400

    fix .sh files

--- master.sh
+++ master.sh
@@ -4,13 +4,11 @@ export SPARK_MASTER_HOST=`hostname`
 
 . "/opt/spark/sbin/spark-config.sh"
 
-#. "/opt/spark/sbin/load-spark-env.sh"
-
 mkdir -p $SPARK_MASTER_LOG
 
 export SPARK_HOME=/opt/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
+cd $SPARK_HOME/bin && $SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
