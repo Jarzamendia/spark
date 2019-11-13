9c527d5650fb333e7bccaaccc61edd75ab3ea5e4
Joao E Arzamendia <jarzamendia@fazenda.ms.gov.br>
2019-11-12 20:48:19 -0400

    remove some css

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 docker build -t jarzamendia/spark-base:latest -f Dockerfile-base .
-docker build -t jarzamendia/spark-master:latest -f Dockerfile-master .
-docker build -t jarzamendia/spark-worker:latest -f Dockerfile-worker .
+docker build -t jarzamendia/spark-master:staging -f Dockerfile-master .
+docker build -t jarzamendia/spark-worker:staging -f Dockerfile-worker .
