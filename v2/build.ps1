48e6211b8f6a8a3d3687e3f5f83fc220a0c2819c
João Ernesto Arzamendia <jearzamendia@gmail.com>
2020-11-18 09:10:47 -0400

    separar em v2 e v3

---
+++ v2/build.ps1
@@ -0,0 +1,3 @@
+docker build -t jarzamendia/spark-base:spark2.4.7-hadoop2.7 -f Dockerfile-base .
+docker build -t jarzamendia/spark-master:spark2.4.7-hadoop2.7 -f Dockerfile-master .
+docker build -t jarzamendia/spark-worker:spark2.4.7-hadoop2.7 -f Dockerfile-worker .
---
+++ v3/worker.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+. "/opt/spark/sbin/spark-config.sh"
+
+mkdir -p $SPARK_WORKER_LOG
+
+export SPARK_HOME=/opt/spark
+
+ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+
+$SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
+ --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
