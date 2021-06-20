e17742b39184b1d8f11c5c9ced3a3f3f7865fb00
Joao E. Arzamendia <jearzamendia@gmail.com>
2021-06-20 14:56:02 -0400

    fix v2 build

--- v2/build.ps1
+++ v2/build.ps1
@@ -1,3 +1,3 @@
-docker build -t jarzamendia/spark-base:spark2.4.7-hadoop2.7 -f Dockerfile-base .
-docker build -t jarzamendia/spark-master:spark2.4.7-hadoop2.7 -f Dockerfile-master .
-docker build -t jarzamendia/spark-worker:spark2.4.7-hadoop2.7 -f Dockerfile-worker .
+docker build -t jarzamendia/spark-base:spark2.4.8-hadoop2.7 -f Dockerfile-base .
+docker build -t jarzamendia/spark-master:spark2.4.8-hadoop2.7 -f Dockerfile-master .
+docker build -t jarzamendia/spark-worker:spark2.4.8-hadoop2.7 -f Dockerfile-worker .
